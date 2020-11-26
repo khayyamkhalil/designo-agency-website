92cb893a2800710d374789d90c68eb3314567293
Khayyam <65266683+khayyamkhalil@users.noreply.github.com>
2020-11-26 11:52:40 -0800

    Added main nav menu for desktop version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,34 +1,8 @@
-// const menuBtn = document.querySelector(".menu-btn");
-// const hamburger = document.querySelector(".menu-btn__burger");
-// const nav = document.querySelector(".nav");
-// const menuNav = document.querySelector(".menu-nav");
-// const navItems = document.querySelectorAll(".menu-nav__item");
-
-// let showMenu = false;
-
-// menuBtn.addEventListener("click", toggleMenu);
-
-// function toggleMenu() {
-//   if (!showMenu) {
-//     hamburger.classList.add("open");
-//     nav.classList.add("open");
-//     menuNav.classList.add("open");
-//     navItems.forEach((item) => item.classList.add("open"));
-//     showMenu = true;
-//   } else {
-//     hamburger.classList.remove("open");
-//     nav.classList.remove("open");
-//     menuNav.classList.remove("open");
-//     navItems.forEach((item) => item.classList.remove("open"));
-//     showMenu = false;
-//   }
-// }
-
-const navToggle = document.querySelector(".nav-toggle");
-const navBurger = document.querySelector(".nav-toggle__burger");
-const navMenu = document.querySelector(".nav__menu");
-const navList = document.querySelector(".nav__list");
-const navItem = document.querySelectorAll(".nav__item");
+const navToggle = document.querySelector(".nav-toggle"); //menu-btn
+const navBurger = document.querySelector(".nav-toggle__burger"); //menu-btn__burger
+const navMenu = document.querySelector(".nav__menu"); //nav
+const navList = document.querySelector(".nav__list"); //menu-nav
+const navItem = document.querySelectorAll(".nav__item"); //menu-nav__item
 
 let showMenu = false;
 
